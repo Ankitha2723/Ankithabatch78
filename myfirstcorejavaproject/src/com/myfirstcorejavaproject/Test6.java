89105a559af6711028e246c9a72c2c7e71b8a750
Ankitha2723 <ankithavanga8@gmail.com>
2026-09-11 10:25:13 +0530

    good morning

---
+++ myfirstcorejavaproject/src/com/myfirstcorejavaproject/Test6.java
@@ -0,0 +1,19 @@
+package com.myfirstcorejavaproject;
+
+public class Test6 {
+	int money=100;
+	int pencilcost=7;
+	int pencils=money/pencilcost;
+	int balance=money%pencilcost;
+
+	public static void main(String[] args) {
+		Test6 t1=new Test6();
+		System.out.println("no of pencils:"+t1.pencils);
+		System.out.println("Balance:"+t1.balance);
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
